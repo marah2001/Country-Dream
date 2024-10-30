989185d41c058638ce4449481e0c7fd78dd12bdf
Marah Alsafadi <marahkinana21@gmail.com>
2024-10-30 03:16:54 +0200

    first commit

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          int counter = 0;
+        while (counter >=20)  {
+            int randomNumber = Random.Range(1, 21);
+            Debug.Log("Hello, this is number " + randomNumber);
+            counter++;
+            
+             if(randomNumber == 5)  continue;
+             else if(randomNumber == 15)  break;
+        
+            Debug.Log(randomNumber);
+          
+         }
+    // Exercise 2
+     string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+        System.Random random = new System.Random();
+        string funnySentence = "";
+        int wordsCount = 7;
+
+        int count = 0;
+        while (count < wordsCount)
+        {
+            int index = random.Next(0, words.Length);
+            funnySentence += words[index] + " "; 
+            count++;
+        }
+
+        Debug.Log(funnySentence); 
+    }
+
+    
+}
